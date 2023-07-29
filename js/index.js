c759b8e91aa8b84cb7949c36d483dfb2b5a81544
yekuz <3336016933@qq.com>
2023-07-29 10:26:11 +0800

    Site updated: 2023-07-29 10:26:02

--- js/index.js
+++ js/index.js
@@ -336,3 +336,71 @@ addLongtabListener(box, popupMenu)
 
 
 
+//导航栏js
+// 返回顶部 显示网页阅读进度
+window.onscroll = percent; // 执行函数
+// 页面百分比
+function percent() {
+  let a = document.documentElement.scrollTop || window.pageYOffset, // 卷去高度
+    b =
+      Math.max(
+        document.body.scrollHeight,
+        document.documentElement.scrollHeight,
+        document.body.offsetHeight,
+        document.documentElement.offsetHeight,
+        document.body.clientHeight,
+        document.documentElement.clientHeight
+      ) - document.documentElement.clientHeight, // 整个网页高度 减去 可视高度
+    result = Math.round((a / b) * 100), // 计算百分比
+    btn = document.querySelector("#percent"); // 获取图标
+
+  result <= 99 || (result = 99), (btn.innerHTML = result);
+}
+
+document.getElementById("page-name").innerText = document.title.split(" | 安知鱼")[0];
+
+categoriesBarActive()
+topCategoriesBarScroll()
+
+//分类条
+function categoriesBarActive(){
+  var urlinfo = window.location.pathname;
+  urlinfo = decodeURIComponent(urlinfo)
+  console.log(urlinfo);
+  //判断是否是首页
+  if (urlinfo == '/'){
+    if (document.querySelector('#category-bar')){
+      document.getElementById('首页').classList.add("select")
+    }
+  }else {
+    // 验证是否是分类链接
+    var pattern = /\/categories\/.*?\//;
+    var patbool = pattern.test(urlinfo);
+    console.log(patbool);
+    // 获取当前的分类
+    if (patbool) {
+      var valuegroup = urlinfo.split("/");
+      console.log(valuegroup[2]);
+      // 获取当前分类
+      var nowCategorie = valuegroup[2];
+      if (document.querySelector('#category-bar')){
+        document.getElementById(nowCategorie).classList.add("select");
+      }
+    }
+  }
+  
+}
+
+//鼠标控制横向滚动
+function topCategoriesBarScroll(){
+  if (document.getElementById("category-bar-items")){
+    let xscroll = document.getElementById("category-bar-items");
+  xscroll.addEventListener("mousewheel", function (e) {
+    //计算鼠标滚轮滚动的距离
+    let v = -e.wheelDelta / 2;
+    xscroll.scrollLeft += v;
+    //阻止浏览器默认方法
+    e.preventDefault();
+}, false);
+  }
+}
